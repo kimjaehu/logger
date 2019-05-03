d17e655a2bb1a29ef54d86e4da6d1ad57de530fc
Jay Kim <kimjaehu@gmail.com>
2019-05-03 02:15:27 -0400

    Login and register page added

--- leadmanager/frontend/src/components/layout/Header.js
+++ leadmanager/frontend/src/components/layout/Header.js
@@ -1,39 +1,79 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
 export class Header extends Component {
+  static propTypes = {
+    auth: PropTypes.object.isRequired,
+    logout: PropTypes.func.isRequired
+  };
+
   render() {
+    const { isAuthenticated, user } = this.props.auth;
+
+    const authLinks = (
+      <ul className='navbar-nav ml-auto mt-2 mt-lg-0'>
+        <span className='navbar-text mr-3'>
+          <strong>{user ? `Welcome [${user.username}]` : ''}</strong>
+        </span>
+        <li className='nav-item'>
+          <button
+            onClick={this.props.logout}
+            className='nav-link btn btn-info btn-sm text-light'
+          >
+            logout
+          </button>
+        </li>
+      </ul>
+    );
+
+    const guestLinks = (
+      <ul className='navbar-nav ml-auto mt-2 mt-lg-0'>
+        <li className='nav-item'>
+          <Link to='/register' className='nav-link'>
+            register
+          </Link>
+        </li>
+        <li className='nav-item'>
+          <Link to='/login' className='nav-link'>
+            login
+          </Link>
+        </li>
+      </ul>
+    );
+
     return (
       <nav className='navbar navbar-expand-sm navbar-light bg-light'>
-        <a className='navbar-brand' href='#'>
-          topilocal
-        </a>
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-toggle='collapse'
-          data-target='#navbarNavAltMarkup'
-          aria-controls='navbarNavAltMarkup'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <span className='navbar-toggler-icon' />
-        </button>
-        <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
-          <div className='navbar-nav'>
-            <a className='nav-item nav-link active' href='#'>
-              Home <span className='sr-only'>(current)</span>
-            </a>
-            <a className='nav-item nav-link' href='#'>
-              Features
-            </a>
-            <a className='nav-item nav-link' href='#'>
-              Pricing
-            </a>
-          </div>
+        <div className='container'>
+          <a className='navbar-brand' href='#'>
+            topilocal
+          </a>
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-toggle='collapse'
+            data-target='#navbarNavAltMarkup'
+            aria-controls='navbarNavAltMarkup'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <span className='navbar-toggler-icon' />
+          </button>
+          <div className='collapse navbar-collapse' id='navbarNavAltMarkup' />
+          {isAuthenticated ? authLinks : guestLinks}
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { logout }
+)(Header);
